f6a5bef8e9ca9e2e90195841aa71a1f27458ee11
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-07-27 16:58:11 +0300

    feature: implement posting translated content automatically

--- scraper/tg_bot.py
+++ scraper/tg_bot.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater
 from dotenv import load_dotenv
 import scraper as SC
 from telegram.constants import ParseMode
+from googletrans import Translator
 import os
 import asyncio
 
@@ -14,10 +15,21 @@ bot = Bot(token=bot_token)
 
 channel_id = "@IspoortiiAddugnaa"
 
+title,content = SC.Scraper()
+
+async def translate_text():
+     async with Translator() as translator:
+         translated_title = await translator.translate(title,src='en', dest='om')
+         translate_content = await translator.translate(content, src='en', dest='om')
+
+         return translated_title.text,translate_content.text
+
+     
+translated_title,translated_content = asyncio.run(translate_text())
+
 async def Send_Message():
     image_path='image.jpg'
-    title,content = SC.Scraper()
-    await bot.send_photo(chat_id=channel_id,photo=open(image_path,'rb'),caption=f'<b>\n{title}</b>',parse_mode=ParseMode.HTML)
-    await bot.send_message(chat_id=channel_id,text=content)
+    await bot.send_photo(chat_id=channel_id,photo=open(image_path,'rb'),caption=f'<b>\n{translated_title}</b>',parse_mode=ParseMode.HTML)
+    await bot.send_message(chat_id=channel_id,text=translated_content)
 
 asyncio.run(Send_Message())    
